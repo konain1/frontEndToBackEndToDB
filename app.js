f3bd428088a264cae6cb1152d78d8c9f25559096
Konain1 <kaunainkasab@gmail.com>
2024-01-26 03:02:44 +0530

    DB added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://konain7:Kaunain%4099@cluster0.rmyvhx6.mongodb.net/facebookpage');
+
+const fbUsers = mongoose.model('fbID', { name: String , email:String,password:String,uniqueId:String });
+
 
 const path = require('path')
 const bodyParser = require('body-parser');
@@ -15,7 +20,7 @@ const staticPath = path.join(__dirname , '/public')
 app.use(express.static(staticPath)); 
 
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async function (req, res){
     const username = req.body.username;
     const userEmail = req.body.email;
     const password = req.body.password
@@ -25,13 +30,25 @@ app.post('/signup', (req, res) => {
     token = jwt.sign({ email: userEmail }, password);
 
 
+    const existingUser = await fbUsers.findOne({ email: userEmail });
+    // console.log(existingUser)
+
+    if (existingUser) {
+        return res.status(400).json({ error: 'User already exists' });
+    }
+    const fbguy = new fbUsers({ name: username,email:userEmail,password:password,uniqueId:token });
+    fbguy.save().then(() => console.log('new user added'));
 
     res.send('Username received: ' + username + " " +' and the email is '+userEmail + " " + token);
 });
 
-app.post('/login', (req, res) => {
+app.post('/login',(req, res) => {
     const userEmail = req.body.loginemail;
     const password = req.body.password;
+
+    
+
+
     if(userEmail == jtwEmail && jtwPassword == password){
         let decoded = jwt.verify(token, password);
         res.send(decoded.email + " you are varified")
